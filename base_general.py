82655221e6fcf3a35a3477653f40fa4e68dbf67b
andrespipe77 <amaolugo2202@gmail.com>
2026-05-10 06:41:34 +0200

    Update base_general.py

--- base_general.py
+++ base_general.py
@@ -6,9 +6,14 @@ from abc import ABC, abstractmethod
 
 class EntidadBase(ABC):
     """
-    Clase abstracta que obliga a implementar validaciones.
+    Clase abstracta que obliga a implementar validaciones y representación.
     """
 
     @abstractmethod
     def validar(self):
+        pass
+
+    # CORREGIDO: Añadido __str__ abstracto para forzar implementación en todas las entidades
+    @abstractmethod
+    def __str__(self):
         pass
